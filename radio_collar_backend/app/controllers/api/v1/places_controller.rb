bd541248f2976473f8921c5242622d6bf58a2518
Stephen Tong <stevetong83@hotmail.com>
2013-05-14 14:49:12 -0700

    url shortener added

--- radio_collar_backend/app/controllers/api/v1/places_controller.rb
+++ radio_collar_backend/app/controllers/api/v1/places_controller.rb
@@ -21,11 +21,12 @@ class Api::V1::PlacesController < Api::V1::ApiController
   # [422] Invalid params
 
   def create
-    place = @user.places.build(name: params[:name], lat: params[:lat], long: params[:long])
-    if place.save
-      render json: {message: "Place has been deleted"}, success: true, status: 200
+    url = Place.set_map_url(params[:name], params[:lat], params[:lat])
+    @place = @user.places.build(name: params[:name], lat: params[:lat], long: params[:long], location_url: url)
+    if @place.save
+      render json: @place.to_json, success: true, status: 200
     else
-      render json: place.errors, success: false, status: 422
+      render json: @place.errors, success: false, status: 422
     end
   end
 
@@ -33,16 +34,16 @@ class Api::V1::PlacesController < Api::V1::ApiController
   # == POST /api/v1/places/:id
   # Authenticate the user and save location
   def show
-    place = Place.find params[:id]
-    render json: place.to_json
+    @place = Place.find params[:id]
+    render json: @place.to_json
   end
 
   ##
   # == DELETE /api/v1/places/:id
   # Authenticate the user and save location
   def destroy
-    place = Place.find params[:id]
-    place.destroy
+    @place = Place.find params[:id]
+    @place.destroy
     render json: {message: "Place has been deleted"}, success: true, status: 200
   end
 end
