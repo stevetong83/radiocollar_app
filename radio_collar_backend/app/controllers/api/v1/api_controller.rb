8e67e9422e208aaac1f17bb38d6df273d767dd2e
rick carlino <rick.carlino@gmail.com>
2013-06-05 20:59:47 -0700

    Added some notes

--- radio_collar_backend/app/controllers/api/v1/api_controller.rb
+++ radio_collar_backend/app/controllers/api/v1/api_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::ApiController < ApplicationController
   before_filter :verify_user
 
   def verify_user
-    @user = User.where(authentication_token: params[:authentication_token]).first
+    #TODO: I changed the .where().first to find_by().
+    @user = User.find_by(authentication_token: params[:authentication_token])
     if @user.nil?
       render json: { errors: "User must be logged in" }, success: false, status: 420
     end
